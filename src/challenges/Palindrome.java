81fbb33ea71850228a509a22e98757f506452280
Thabang Gideon Magaola <gideon877@live.com>
2019-06-02 15:19:37 +0200

    added functionality

---
+++ src/challenges/Palindrome.java
@@ -0,0 +1,40 @@
+package challenges;
+/*
+* Learned how to:
+ *   - use StringBuilder
+ *   - use split
+ *   - use array indexes
+ *         * to reverse letters
+ *   - use for loop
+ *   - use equalsIgnoreCase
+ *   - use toString()
+* */
+public class Palindrome {
+
+    public boolean reverseWord(String word) {
+        StringBuilder builder = new StringBuilder();
+        String[] letters = word.split("");
+        for (int i = 0; i < letters.length; i++) {
+            builder.append(letters[letters.length - (i+1)]);
+        }
+        return builder.toString().equalsIgnoreCase(word);
+    }
+
+
+    public boolean reverseNumber(int number) {
+        //todo: reverse numbers
+        return true;
+    }
+
+    public boolean reversePhrase(String phrase) {
+        StringBuilder builder = new StringBuilder();
+        phrase = phrase.replaceAll("\\s", "");
+        phrase = phrase.replaceAll("\\'", "");
+
+        String[] letters = phrase.split("");
+        for (int i = 0; i < letters.length; i++) {
+            builder.append(letters[letters.length - (i+1)]);
+        }
+        return builder.toString().equalsIgnoreCase(phrase);
+    }
+}
---
+++ tests/challenges/PalindromeTest.java
@@ -0,0 +1,82 @@
+package challenges;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PalindromeTest {
+
+    @Nested
+    @DisplayName("ss")
+    class ReverseWord {
+
+        @Test
+        void shouldReturnTrue() {
+            Palindrome palindrome = new Palindrome();
+
+            assertEquals(true, palindrome.reverseWord("civic"));
+            assertEquals(true, palindrome.reverseWord("Kayak"));
+            assertEquals(true, palindrome.reverseWord("Racecar"));
+        }
+
+        @Test
+        void shouldReturnFalse() {
+            Palindrome palindrome = new Palindrome();
+
+            assertEquals(false, palindrome.reverseWord("thabang"));
+            assertEquals(false, palindrome.reverseWord("gideon"));
+            assertEquals(false, palindrome.reverseWord("King"));
+        }
+    }
+
+
+    @Nested
+    @DisplayName("ss")
+    class ReversePhrase {
+
+        @Test
+        void shouldReturnTrue() {
+            Palindrome palindrome = new Palindrome();
+
+            assertEquals(true, palindrome.reversePhrase("Don't nod"));
+            assertEquals(true, palindrome.reversePhrase("I did, did I"));
+            assertEquals(true, palindrome.reversePhrase("My gym"));
+        }
+
+        @Test
+        void shouldReturnFalse() {
+            Palindrome palindrome = new Palindrome();
+
+            assertEquals(false, palindrome.reversePhrase("Where are you"));
+            assertEquals(false, palindrome.reversePhrase("I did not"));
+            assertEquals(false, palindrome.reversePhrase("we house Sunday"));
+        }
+    }
+
+    @Nested
+    @DisplayName("ss")
+    @Disabled("Method need implementation")
+    class ReverseNumber {
+
+        @Test
+        void shouldReturnTrue() {
+            Palindrome palindrome = new Palindrome();
+
+            assertEquals(true, palindrome.reverseNumber(2112));
+            assertEquals(true, palindrome.reverseNumber(121));
+            assertEquals(true, palindrome.reverseNumber(1991));
+        }
+
+        @Test
+        void shouldReturnFalse() {
+            Palindrome palindrome = new Palindrome();
+
+            assertEquals(false, palindrome.reverseNumber(10111));
+            assertEquals(false, palindrome.reverseNumber(123456789));
+            assertEquals(false, palindrome.reverseNumber(010101));
+        }
+    }
+}
