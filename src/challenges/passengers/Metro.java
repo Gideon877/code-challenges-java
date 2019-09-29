0d911c55e6a2dbdada5d69227b6acfd434b4c469
Thabang Gideon Magaola <gideon877@live.com>
2019-09-29 12:45:18 +0200

    bus passengers count

---
+++ src/challenges/passengers/Metro.java
@@ -0,0 +1,11 @@
+package challenges.passengers;
+
+import java.util.ArrayList;
+
+public class Metro {
+    public static int countPassengers(ArrayList<int[]> stops) {
+        return stops.stream()
+                .mapToInt(x -> x[0] - x[1])
+                .sum();
+    }
+}
---
+++ tests/challenges/passengers/MetroTest.java
@@ -0,0 +1,21 @@
+package challenges.passengers;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MetroTest {
+    Metro metro = new Metro();
+
+    @Test
+    public void test1() {
+        ArrayList<int[]> list = new ArrayList<int[]>();
+        list.add(new int[] {10,0});
+        list.add(new int[] {3,5});
+        list.add(new int[] {2,5});
+        assertEquals(5, metro.countPassengers(list));
+    }
+
+}
